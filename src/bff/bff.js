40283a3be211095f33da1bd4f0bd87b964511ad8
paha1985 <paha290585@gmail.com>
2024-09-13 16:14:31 +0800

    Создать BFF

---
+++ src/bff/bff.js
@@ -0,0 +1,45 @@
+import { addUser } from './add-user';
+import { createSession } from './create-session';
+import { getUser } from './get-user';
+
+export const server = {
+	async authotize(authLogin, authPassword) {
+		const user = getUser(authLogin);
+
+		if (!user) {
+			return {
+				error: 'Такой пользователь не найден',
+				res: null,
+			};
+		}
+
+		if (!authPassword !== user.password) {
+			return {
+				error: 'Неверный пароль',
+				res: null,
+			};
+		}
+
+		return {
+			error: null,
+			res: createSession(user.role_id),
+		};
+	},
+	async register(regLogin, regPassword) {
+		const user = getUser(regLogin);
+
+		if (user) {
+			return {
+				error: 'Такой логин уже занят',
+				res: null,
+			};
+		}
+
+		await addUser(regLogin, regPassword);
+
+		return {
+			error: null,
+			res: createSession(user.role_id),
+		};
+	},
+};
